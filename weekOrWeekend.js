70997554ae1eb0fe178b783cd6699f594e5973f3
Mzizio <mtshalimxolisii@gmail.com>
2017-02-02 10:08:37 +0200

    Update weekOrWeekend.js

--- weekOrWeekend.js
+++ weekOrWeekend.js
@@ -1,1 +1,38 @@
 
+const assert = require('assert');
+
+//test passing
+assert.equal(weekOrWeekend('Wednesday'), 'week');
+assert.equal(weekOrWeekend('Sunday'), 'weekend');
+
+//This function takes in a day parameter. If the day passed in is a weekDay it should return 'week' otherwise is should return 'weekend';
+function weekOrWeekend(day){
+  
+  switch(day){
+    case "Monday":
+            return "week";
+      break;
+    case "Tuesday":
+            return "week";
+      break;
+    case "Wednesday":
+            return "week";
+      break;
+    case "Thursday":
+            return "week";
+      break;
+    case "Friday":
+            return "week";
+      break;
+    case "Saturday":
+            return "weekend";
+      break;
+    case "Sunday":
+            return "weekend";
+      break;
+      
+    default:
+      return "Invalid day";
+            }
+
+};
